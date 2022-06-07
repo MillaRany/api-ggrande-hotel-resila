4209d469b749431c27acba682b02bc6d0c1d698f
milla <milla_rany@hotmail.com>
2022-06-07 17:47:01 -0300

    alteração

--- src/projeto.js
+++ src/projeto.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
 //importando controller
 const diaria = require('./controllers/diaria-controller.js')
 //importando BD
@@ -12,8 +11,8 @@ app.use(express.json())
 //chamando controller e passando o express
 diaria(app, bd)
 
-let = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
+app.listen( port , ()=>{
     console.log("rodando na porta 3000")
 }) 
